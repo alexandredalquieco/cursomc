45243bcf198863c29a9ec6fe7138c6b4e59bcdab
alexandredalquieco <alexandre.dalquieco@gmail.com>
2020-11-10 09:47:59 -0300

    Validation

--- src/main/java/com/dalquieco/cursomc/services/CategoriaService.java
+++ src/main/java/com/dalquieco/cursomc/services/CategoriaService.java
@@ -1,11 +1,17 @@
 package com.dalquieco.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dalquieco.cursomc.domain.Categoria;
+import com.dalquieco.cursomc.dto.CategoriaDTO;
 import com.dalquieco.cursomc.repositories.CategoriaRepository;
 import com.dalquieco.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,4 +26,36 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try { repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Nao [e possivel excluir Categoria de possui produtod");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getName());
+	}
 }
